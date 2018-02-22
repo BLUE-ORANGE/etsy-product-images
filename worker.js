cd5004c2f8c06b32a1550044e33a9b2d39b565a8
Trevor Old <trevor.old@outlook.com>
2018-02-22 15:46:56 -0800

    Grabbed 200 + kitten images from imgur

--- worker.js
+++ worker.js
@@ -9,7 +9,7 @@ let getImages = () => {
   let options = (page) => {
     return {
       url: `https://api.imgur.com/3/gallery/search/top/{{window}}/${page}/`,
-      qs: { q_type: 'jpg', q: 'kitten' },
+      qs: { q_type: 'jpg', q: 'cute kitten' },
 
       headers: {
         Authorization: `Client-ID ce6360e2634f0e7`
@@ -38,6 +38,15 @@ let getImages = () => {
                 .then((cats) => {
                   cats = JSON.parse(cats);
                   writeImagesToFile(cats);
+                  rp(options(5))
+                    .then((cute) => {
+                      cute = JSON.parse(cute);
+                      writeImagesToFile(cute);
+                      imagesCount += cute.data.length;
+                    })
+                    .catch((err) => {
+                      console.log(`err on 5th page: ${err}`)
+                    })
 
                 })
                 .catch((err) => {
@@ -61,19 +70,24 @@ let getImages = () => {
 
 let writeImagesToFile = (obj) => {
   obj.data.forEach((pic) => {
-    var picture = {
-      id: pic.id,
-      title: pic.title,
-      link: pic.link
-    }
-    fs.appendFileAsync('data.json', JSON.stringify(picture, null, 2) +',' + '\n')
-      .then((data) => {
-        console.log(`File saved: ${picture.title}`);
-      })
-      .catch((err) => {
-        console.log(`error saving file: ${err}`);
-      })
-  })
+    if (!pic.link.includes('.jpg')) {
+      console.log(`${pic.link} doesn't include jpg, skipping`)
+    } else {
+
+      var picture = {
+        id: pic.id,
+        title: pic.title,
+        link: pic.link
+      }
+      fs.appendFileAsync('data.json', JSON.stringify(picture, null, 2) +',' + '\n')
+        .then((data) => {
+          console.log(`File saved: ${picture.title}`);
+        })
+        .catch((err) => {
+          console.log(`error saving file: ${err}`);
+        })
+      }
+    })
 }
 
 

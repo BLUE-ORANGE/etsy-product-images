16e3c970537e772c3b3f86210ea54c7cdbee2a6d
Trevor Old <trevor.old@outlook.com>
2018-02-25 16:22:17 -0800

    HTTP response is now wrapped in an object containing a results prop

--- server/routes.js
+++ server/routes.js
@@ -2,25 +2,17 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database/index.js');
 
+
 router.get('/:id/images', (req, res) => {
-  var id = req.params.id;
-  if (id) {
-    db.getImageById(id, (err, data) => {
-      if (err) {
-        res.statusCode(404);
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    })
-  }
-});
-router.get('/:id', (req, res) => {
   var id = req.params.id;
   if (id) {
     db.getImagesForProduct(id)
     .then((data) => {
-      res.send(data);
+      let resp = {
+        results: []
+      }
+      resp.results = data;
+      res.send(resp);
     })
     .catch((err) => {
       res.statusCode = 404;

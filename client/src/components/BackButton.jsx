6cc221455a9d4d53c354a821c000164735b30152
Trevor Old <trevor.old@outlook.com>
2018-02-26 19:43:06 -0800

    centered on images

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const BackButton = (props) => {
-  const { className, style, onClick } = props;
+  // const { className, style, onClick } = props;
   return (
-    <div id="image-left-arrow-container" className="image-arrow-box-container">
-      <button onClick={onClick} id="image-left-arrow" className="image-arrow-box" aria-label="Previous image">
-        <div className="arrow">◅</div>
-      </button>
-    </div>
+    // <div>
+    //   <button {...props} aria-label="Previous image">
+    //     <div className="arrow">◅</div>
+    //   </button>
+    // </div>
+    <button {...props} />
+    // ◅
+    
   );
 };
 

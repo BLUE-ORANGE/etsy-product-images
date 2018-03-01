e37e8e0d70c03eb0f2358082410efca472cddf7f
Trevor Old <trevor.old@outlook.com>
2018-03-01 12:11:18 -0800

    loading fonts

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const BackButton = (props) => {
+const BackButton = props =>
   // const { className, style, onClick } = props;
-  return (
+  (
     <div {...props} />
-    // ◅
+  // ◅
   );
-};
-
 export default BackButton;

05214b9b4644d2b656f4342796f19b7a821e2874
Trevor Old <trevor.old@outlook.com>
2018-03-04 13:40:36 -0800

    clean up commented html

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
 const BackButton = props =>
-  // const { className, style, onClick } = props;
 
-    // <div>
-    //   <button {...props} aria-label="Previous image">
-    //     <div className="arrow">◅</div>
-    //   </button>
-    // </div>
   <button {...props} />;
-    // ◅
 
 
 export default BackButton;

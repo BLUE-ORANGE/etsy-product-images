19470931ae349639356d3319ec972c1a1fca2bc4
Trevor Old <trevor.old@outlook.com>
2018-03-04 13:39:14 -0800

    fix bad merge of backbutton

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const BackButton = props =>
   // const { className, style, onClick } = props;
-  return (
+
     // <div>
     //   <button {...props} aria-label="Previous image">
     //     <div className="arrow">◅</div>
     //   </button>
     // </div>
-    <button {...props} />
+  <button {...props} />;
     // ◅
 
-  );
+
 export default BackButton;

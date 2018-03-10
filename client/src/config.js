5c87a117a825953cf95ba1589bf9014b8fc67cdf
Trevor Old <trevor.old@outlook.com>
2018-03-10 15:55:31 -0800

    reverted deletion of seedatabase and added config file

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@ const apiVersion = 'v1';
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === 'localhost') {
-  backendHost = 'http://localhost:8001';
+  backendHost = 'http://localhost:8000';
 }
 
 exports.API_ROOT = `${backendHost}/${apiVersion}`;
